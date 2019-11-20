2564f4c7647e27bb3f4098474e224f8a8b2313ab
Prakasha, Prashanth <prashanth.prakasha@philips.com>
2019-11-20 08:30:59 +0530

    c

--- js/helper.js
+++ js/helper.js
@@ -21,31 +21,31 @@ function App(client) {
     this.client = client;
 }
 
-function fetchCurrentPatient() {
+App.prototype.fetchCurrentPatient = function () {
     var render = createRenderer("patient");
     render("Loading...");
     return this.client.patient.read().then(render, render);
 };
 
-function fetchCurrentEncounter() {
+App.prototype.fetchCurrentEncounter = function () {
     var render = createRenderer("encounter");
     render("Loading...");
     return this.client.encounter.read().then(render, render);
 };
 
-function fetchCurrentUser() {
+App.prototype.fetchCurrentUser = function () {
     var render = createRenderer("user");
     render("Loading...");
     return this.client.user.read().then(render, render);
 };
 
-function request(requestOptions, fhirOptions) {
+App.prototype.request = function (requestOptions, fhirOptions) {
     var render = createRenderer("output");
     render("Loading...");
     return this.client.request(requestOptions, fhirOptions).then(render, render);
 };
 
-function renderContext() {
+App.prototype.renderContext = function () {
     return Promise.all([
         this.fetchCurrentPatient(),
         this.fetchCurrentUser(),
@@ -53,7 +53,7 @@ function renderContext() {
     ]);
 };
 
-function setLabel(containerId, label) {
+App.prototype.setLabel = function (containerId, label) {
     document.getElementById(containerId).previousElementSibling.innerText = label;
 };
 
